2622442656437fa9560d47e35a8a3becea1a57b5
axemc <192119741@qq.com>
2021-12-02 00:09:05 +0800

    feature: spring example-00

---
+++ basic/jdk/concurrence/src/main/java/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        int processors = Runtime.getRuntime().availableProcessors();
+        System.out.println(processors);
+    }
+}
