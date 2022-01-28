d32be7aa33db7c66979041c02b095d51b3efb8dd
axemc <192119741@qq.com>
2022-01-28 11:26:12 +0800

    feature: update UnsafeUtils

--- basic/jdk17/src/main/java/main/internal/UnsafeTest.java
+++ basic/jdk17/src/main/java/main/internal/UnsafeTest.java
@@ -3,6 +3,5 @@ package main.internal;
 public class UnsafeTest {
     public static void main(String[] args) {
 //        Unsafe instance = UnsafeUtils.getInstance();
-//        System.out.println(instance);
     }
 }
--- basic/jdk17/src/main/java/main/utils/UnsafeUtils.java
+++ basic/jdk17/src/main/java/main/utils/UnsafeUtils.java
@@ -1,20 +1,12 @@
 package main.utils;
 
-import lombok.SneakyThrows;
-
 public class UnsafeUtils {
 
-//    private static Unsafe UNSAFE;
-
-    @SneakyThrows
-    private UnsafeUtils() {
+//    @SneakyThrows
+//    public static Unsafe getInstance() {
 //        Field field = Unsafe.class.getDeclaredField("theUnsafe");
 //        field.setAccessible(true);
-//        UNSAFE = (Unsafe) field.get(null);
-    }
-
-//    public static Unsafe getInstance() {
-//        return UNSAFE;
+//        return (Unsafe) field.get(null);
 //    }
 
 }
