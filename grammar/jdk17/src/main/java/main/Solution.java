19f89d6dcfbf35a2ac0eb7d1736204c947a41c8c
axemc <192119741@qq.com>
2022-05-10 20:54:46 +0800

    calculate more size count string

--- grammar/jdk17/src/main/java/main/Solution.java
+++ grammar/jdk17/src/main/java/main/Solution.java
@@ -1,9 +1,46 @@
 package main;
 
+import lombok.SneakyThrows;
+
+import java.io.FileInputStream;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
 /**
  * @author: axemc
  * @dateTime: 2022/04/25 22:13:52
  */
 public class Solution {
+
+    static class Node {
+        String value;
+        int count;
+        Node(String string, int count) {
+            this.value = string;
+            this.count = count;
+        }
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        FileInputStream inputStream = new FileInputStream("content.txt");
+        String content = new String(inputStream.readAllBytes());
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (String str : content.split(",")) {
+            if (hashMap.containsKey(str)) {
+                hashMap.put(str, hashMap.get(str) + 1);
+            } else {
+                hashMap.put(str, 1);
+            }
+        }
+
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(a -> a.count));
+        hashMap.forEach((key, value) -> priorityQueue.add(new Node(key, value)));
+        while (!priorityQueue.isEmpty()) {
+            System.out.println(priorityQueue.poll().value + "," + Objects.requireNonNull(priorityQueue.poll()).value);
+        }
+    }
 
 }
